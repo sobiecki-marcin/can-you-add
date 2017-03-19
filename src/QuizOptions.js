0acef9f540543e2b8228498bce72128d23593388
marcin-sobiecki <marcin.sobiecki@me.com>
2017-03-19 13:41:35 +0100

    Adds QuizOptions component

---
+++ src/QuizOptions.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class QuizOptions extends Component {
+
+    render(){
+        return(
+            <div className='fields'><div className='field-block'>10</div></div> 
+        );
+    }
+}
+
+export default QuizOptions;
