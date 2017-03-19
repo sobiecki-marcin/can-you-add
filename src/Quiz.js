654f29e2552b9bfe806d2f8303af14bc0ef3d2ed
marcin-sobiecki <marcin.sobiecki@me.com>
2017-03-19 14:42:54 +0100

    Adds game logic

--- src/Quiz.js
+++ src/Quiz.js
@@ -7,18 +7,58 @@ class Quiz extends Component {
     constructor(props) {
         super(props);
 
-        let riddle = {
-            resultsArray: [8,9,10,11],
-            field1: 5,
-            field2: 5,
-            answer: 10
-        };
+        let riddle = this.playGame();
 
         this.state = {riddle};
 
         this.renderOptions = this.renderOptions.bind(this);
     }
+    randomNumber(min, max) {
+        return Math.floor(Math.random() * (max-min+1))+min;
+    }
+    generateRandomOptions(sum) {
+        let resultsArray = [];
+        let randomNumberArray = [];
 
+        while (randomNumberArray.length <= 3) {
+            let randomNumber = this.randomNumber(1, 19);
+            if(randomNumberArray.indexOf(randomNumber) > -1) continue;
+            randomNumberArray.push(randomNumber);
+        }
+
+        for(let i=0; i<3; i++){
+        let addSubtract = this.randomNumber(0,1);
+        let result = sum;
+        if(addSubtract ===1 ){
+            //add the number to the result
+            result += randomNumberArray[i];
+            resultsArray.push(result);
+        } else {
+            //subtract the number from the result
+            result -= randomNumberArray[i];
+            resultsArray.push(result);
+        }
+        }
+
+
+        return resultsArray;
+    }
+    playGame() {
+        let field1 = this.randomNumber(20,50);
+        let field2 = this.randomNumber(20,50);
+        let result = field1 + field2;
+        let resultsArray = this.generateRandomOptions(result)
+        resultsArray.push(result);
+        resultsArray.sort(function(a,b) {return 0.5 - Math.random()});
+        console.log(resultsArray);
+        let riddle = {
+            resultsArray: resultsArray,
+            field1: field1,
+            field2: field2,
+            answer: result
+        };
+        return riddle;
+    }
     renderOptions() {
         return(
             <div className='options'>
@@ -33,7 +73,7 @@ class Quiz extends Component {
         return (
             <div className='quiz'>
                 <div className='quiz-content'>
-                    <p className='question'>What is the sum of <span className='text-info'>5</span> and <span className='text-info'>5</span> ?</p>
+                    <p className='question'>What is the sum of <span className='text-info'>{this.state.riddle.field1}</span> and <span className='text-info'>{this.state.riddle.field2}</span> ?</p>
                    {this.renderOptions()}
                    
                   
